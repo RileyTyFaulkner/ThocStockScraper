a2398ad167c78c7a22beadbba354b5a5f11e4356
RileyTyFaulkner <81701405+RileyTyFaulkner@users.noreply.github.com>
2022-02-15 20:34:48 -0500

    Update ThocStockScraper.py
    Editing code to how it appears on my personal machine (raspberryPi running script with Cron)
    -Removed time loop
    -Stated __main__

--- ThocStockScraper.py
+++ ThocStockScraper.py
@@ -4,67 +4,66 @@ import numpy #For comparing New Stock List and Old Stock List
 import os #For overwriting Old stock with evaluated New stock
 import time
 import io
+def main():
+    def getCurrentStock(type):
+        global matches
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-GPC': '1',
+        'DNT': '1',
+        'Pragma': 'no-cache',
+        'Cache-Control': 'no-cache',
+        'TE': 'Trailers',
+        }
+        response = requests.get('https://thocstock.com/'+type, headers=headers)
+        uncleaned = response.text
+        pattern =r'<h2 class=\"text-lg\">\s*.*?\s*>\s*(.*?)\n\s*<\/a>'
+        matches = re.findall(pattern,uncleaned)
+        listToStr = map(str,matches)
+        stringForFile = '\n'.join(listToStr)
+        fileName = "New {} Stock.txt".format(type)
+        with io.open(fileName, "w", encoding="utf-8") as file:
+            file.write(stringForFile)
+        #Old Saving method, got mad at some characters.
+        #with open(fileName, "w") as file:
+        #    file.write(stringForFile)
 
-def getCurrentStock(type):
-    global matches
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:87.0) Gecko/20100101 Firefox/87.0',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-    'Accept-Language': 'en-US,en;q=0.5',
-    'Connection': 'keep-alive',
-    'Upgrade-Insecure-Requests': '1',
-    'Sec-GPC': '1',
-    'DNT': '1',
-    'Pragma': 'no-cache',
-    'Cache-Control': 'no-cache',
-    'TE': 'Trailers',
-    }
-    response = requests.get('https://thocstock.com/'+type, headers=headers)
-    uncleaned = response.text
-    pattern =r'<h2 class=\"text-lg\">\s*.*?\s*>\s*(.*?)\n\s*<\/a>'
-    matches = re.findall(pattern,uncleaned)
-    listToStr = map(str,matches)
-    stringForFile = '\n'.join(listToStr)
-    fileName = "New {} Stock.txt".format(type)
-    with io.open(fileName, "w", encoding="utf-8") as file:
-        file.write(stringForFile)
-    #Old Saving method, got mad at some characters.
-    #with open(fileName, "w") as file:
-    #    file.write(stringForFile)
+    def overwriteOldStock(type):
+        oldFile = "Old {} Stock.txt".format(type)
+        newFile = "New {} Stock.txt".format(type)
+        os.remove(oldFile)
+        os.rename(newFile,oldFile)
 
-def overwriteOldStock(type):
-    oldFile = "Old {} Stock.txt".format(type)
-    newFile = "New {} Stock.txt".format(type)
-    os.remove(oldFile)
-    os.rename(newFile,oldFile)
+    def getOldStock(type):
+        #with open("Old {} Stock.txt".format(type)) as file:
+        #    oldStock = file.read().split('\n')
+        with io.open("Old {} Stock.txt".format(type), "r", encoding="utf-8") as file:
+            oldStock = file.read().split('\n')
+        return(oldStock)
 
-def getOldStock(type):
-    #with open("Old {} Stock.txt".format(type)) as file:
-    #    oldStock = file.read().split('\n')
-    with io.open("Old {} Stock.txt".format(type), "r", encoding="utf-8") as file:
-        oldStock = file.read().split('\n')
-    return(oldStock)
+    def findDifference(type):
+        oldStock = getOldStock(type)
+        currentStock = matches
+        difference = numpy.setdiff1d(currentStock,oldStock)
+        if len(difference)>0:
+            print("{}: New Stock found! New items:".format(type))
+            print(difference)
+            #print("Old Stock: {}".format(oldStock))
+            #print("New Stock: {}".format(currentStock))
+            print("Old Stock overwritten.")
+            overwriteOldStock(type)
+        else:
+            print("{}: No new Stock, old stock overwritten.".format(type))
+            overwriteOldStock(type)
 
-def findDifference(type):
-    oldStock = getOldStock(type)
-    currentStock = matches
-    difference = numpy.setdiff1d(currentStock,oldStock)
-    if len(difference)>0:
-        print("{}: New Stock found! New items:".format(type))
-        print(difference)
-        #print("Old Stock: {}".format(oldStock))
-        #print("New Stock: {}".format(currentStock))
-        print("Old Stock overwritten.")
-        overwriteOldStock(type)
-    else:
-        print("{}: No new Stock, old stock overwritten.".format(type))
-        overwriteOldStock(type)
-
-types = ['keycaps', 'accessories', 'deskpads', 'keyboards', 'pcbs', 'stabs', 'switches', 'switch-mod']
-
-#This could be done with the Schedule module
-x = 1
-while x:
+    types = ['keycaps', 'accessories', 'deskpads', 'keyboards', 'pcbs', 'stabs', 'switches', 'switch-mod']
+    
     for stock in types:
         getCurrentStock(stock)
         findDifference(stock)
-    time.sleep(60)
+        
+if __name__ == "__main__":
+    main()
